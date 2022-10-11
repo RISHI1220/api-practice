f6e57c9aa9000f26f05cdb0821d127a5f8646cef
Rishi <Rishibarua08@gmail.com>
2022-10-11 16:19:33 +0530

    Using resources

---
+++ app/controllers/api/movies_controller.rb
@@ -0,0 +1,26 @@
+class Api::MoviesController < ApplicationController
+    def index
+        render json: Movie.all
+    end
+    def create
+        @movie=Movie.create(title: params[:title],producer: params[:producer])
+        render json: Movie.all
+    end
+    def update
+        @movie=Movie.find(params[:id])
+        @movie.update_attributes(movie_params)
+        render json: Movie.all
+    end
+    def destroy
+        Movie.destroy(params[:id])
+        render json: Movie.all
+    end
+    def show
+        @movie=Movie.find(params[:id])
+        render json: @movie
+    end
+    private
+    def movie_params
+        params.permit(:title,:producer)
+    end
+end
