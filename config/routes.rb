04a5f788a19dc7a4679b22ac4fc4b481d90aab17
Rishi <Rishibarua08@gmail.com>
2022-10-11 18:24:59 +0530

    better routing and parameter understanding

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,30 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     # resources :movies, only: [:show, :create, :index, :update, :destroy]    
+    # show all
     get 'movies', to: 'movies#index'
-    post 'movies', to: 'movies#create'
-    get 'movies/:id', to: 'movies#show'
-    put 'movies/:id', to: 'movies#update'
-    patch 'movies/:id', to: 'movies#update'
-    delete 'movies/:id', to: 'movies#destroy'
+
+    # Show perticular using query or body parameter 
+    get 'show', to: 'movies#show'
+    # Show perticular using url parameter
+    get 'show/:id', to: 'movies#show'
+
+    # Create new record and pass parameters in body or query parameter
+    post 'create', to: 'movies#create'
+    
+    # Update record using url id parameter and attrabute parameter in body or query
+    put 'update/:id', to: 'movies#update'
+    patch 'update/:id', to: 'movies#update'
+    # Update record using id from query parameter or attrabutes parameters in body
+    put 'update', to: 'movies#update'
+    patch 'update', to: 'movies#update'
+    
+    # Delete record using if from url parameter
+    delete 'delete/:id', to: 'movies#destroy'
+    # Delete record using query or body paramter
+    delete 'delete', to: 'movies#destroy'
+
+
+    # Note id query parameter can also be send from body if not used strong parameter *******
   end
 end
